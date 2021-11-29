c7e88a7be06df9e46e35f7da5ebd883365740a5c
peter1421 <51041311+peter1421@users.noreply.github.com>
2021-11-29 11:43:58 +0800

    rr

---
+++ src/main/java/fcu/sep/fcushop/FcushopApplication.java
@@ -0,0 +1,21 @@
+package fcu.sep.fcushop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcushopApplication {
+
+  public static void main(String[] args) {
+
+    SpringApplication.run(FcushopApplication.class, args);
+    System.out.println("-------------------------------------------");
+    System.out.println("-------------------------------------------");
+    System.out.println("-------------------------------------------");
+    System.out.println("-------------------------------------------");
+    System.out.println("新的開始");
+  }
+
+}
+//try git
+//http://localhost:8080/
