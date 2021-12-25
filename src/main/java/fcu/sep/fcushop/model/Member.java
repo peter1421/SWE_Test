bded426c9bd3a6492930a4bdc243b389c89ff530
peter1421 <51041311+peter1421@users.noreply.github.com>
2021-12-26 02:14:55 +0800

    會員功能建立(未關聯)

---
+++ src/main/java/fcu/sep/fcushop/model/Member.java
@@ -0,0 +1,60 @@
+package fcu.sep.fcushop.model;
+
+public class Member {
+	private String email;
+	private String name;
+	private String imageUrl;
+	private String phoneNumber;
+	private String address;
+
+	public Member() {
+
+	}
+
+	public Member(String email, String name, String imageUrl, String phoneNumber, String address) {
+		this.email = email;
+		this.name = name;
+		this.imageUrl = imageUrl;
+		this.phoneNumber = phoneNumber;
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public void setImageUrl(String imageUrl){
+		this.imageUrl=imageUrl;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
