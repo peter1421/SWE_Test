88bdd0adc3731117f025b08fc4bda1d06d728b1a
peter1421 <51041311+peter1421@users.noreply.github.com>
2021-12-26 14:16:22 +0800

    登入儲存功能完成

--- src/main/java/fcu/sep/fcushop/controller/AccountController.java
+++ src/main/java/fcu/sep/fcushop/controller/AccountController.java
@@ -6,6 +6,10 @@ import fcu.sep.fcushop.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @RestController
@@ -21,8 +25,7 @@ public class AccountController {
 	}
 
 	//登入相關
-	@PostMapping("/api/login")
-	@ResponseBody
+
 	//登入檢查
 	public Boolean checkAccount(Account account) {
 		if (accountManager.checkLogin(account) == null) {
@@ -34,21 +37,28 @@ public class AccountController {
 		return true;
 
 	}
-
-	public Account getLoginData(@RequestParam String email, @RequestParam String password) {
+	@PostMapping("/api/login")
+	@ResponseBody
+	public boolean getLoginData(@RequestParam String email, @RequestParam String password, HttpServletRequest req, HttpServletResponse resp) {
 		//接收前端帳密資料
 		System.out.println("email is " + email);
 		System.out.println("password is " + password);
 		Account account = new Account(email, password);
+
 		//檢查帳密正確
 		//checkAccount(account);
 		if (accountManager.checkLogin(account) == null) {
 			System.out.println("null");
-			return new Account("null", "null");
+			return false;
 			//錯誤
+		} else {
+			Cookie cookie = new Cookie("email", email);
+			cookie.setPath("/");
+			resp.addCookie(cookie);
+			//相關說明:https://www.uj5u.com/ruanti/1020.html
+			return true;
+			//比對成功
 		}
-		return account;
-		//比對成功
 	}
 
 
@@ -87,7 +97,7 @@ public class AccountController {
 	public String singUp(@RequestParam String email, @RequestParam String name, @RequestParam String imageUrl, @RequestParam String phoneNumber, @RequestParam String address) {
 		System.out.println("email is " + email);
 		//if(後端檢查註冊_function()==ture&&檢查重複_function()==ture).........
-		Member member = new Member(email,name,imageUrl,phoneNumber,address);
+		Member member = new Member(email, name, imageUrl, phoneNumber, address);
 		//連線資料庫
 		accountManager.addMember(member);
 		return "ok";
