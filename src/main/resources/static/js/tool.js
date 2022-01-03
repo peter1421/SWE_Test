b09f436b2d2965a9233567bfcdf38baa1ea65ed2
D0993423 <lee41451@gmail.com>
2022-01-04 03:05:49 +0800

    登入成功後到SUCESS

--- src/main/resources/static/js/tool.js
+++ src/main/resources/static/js/tool.js
@@ -29,6 +29,19 @@
         alert("登出完成")
     }
 
+     function deleteAllCookie() {
+            var cookies = document.cookie.split(";");
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = cookies[i];
+                var eqPos = cookie.indexOf("=");
+                var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+                document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+            }
+            console.log("cookies 清除完成");
+            alert("登出完成")
+            document.location.href="index1.html";
+        }
+
 
 
 
