75612eb8b59b2e4c9347dff598a0b6a9a9e53d73
mark46wei <ohbom292@gmail.com>
2022-01-09 17:20:39 +0800

    Checkstyle修正TestController格式

--- src/main/java/fcu/sep/fcushop/controller/TestController.java
+++ src/main/java/fcu/sep/fcushop/controller/TestController.java
@@ -1,30 +1,37 @@
 
 package fcu.sep.fcushop.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
+/**
+ * 註解起來就好了啦.
+ *
+ *
+ *
+ *
+ *
+ */
 @RestController
 public class TestController {
 
-	@GetMapping("/greeting")
-	public String sayHello(@RequestParam String name) {
-		return "Hello " + name;
-	}
+  @GetMapping("/greeting")
+  public String sayHello(@RequestParam String name) {
+    return "Hello " + name;
+  }
 
-	@GetMapping("/api/cookie1")
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp){
-		//創建cookie
-		Cookie cookie = new Cookie("name", "zhangsan");
-		resp.addCookie(cookie);
-		//return "ok";
-	}
+  @GetMapping("/api/cookie1")
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+    //創建cookie
+    Cookie cookie = new Cookie("name", "zhangsan");
+    resp.addCookie(cookie);
+    //return "ok";
+  }
 }
