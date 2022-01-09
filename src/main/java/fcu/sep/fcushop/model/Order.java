0b16bb265d0676b717ef4cc2a7fff9a1117b6889
mark46wei <ohbom292@gmail.com>
2022-01-09 21:11:08 +0800

    Checkstyle修正Order格式

--- src/main/java/fcu/sep/fcushop/model/Order.java
+++ src/main/java/fcu/sep/fcushop/model/Order.java
@@ -1,62 +1,78 @@
 package fcu.sep.fcushop.model;
 
+/**
+ * 註解起來就好了啦.
+ *
+ *
+ *
+ *
+ *
+ */
 public class Order {
-	private int productId;
-	private String buyerEmail;
-	private int count;
-	private String state;
-	private int billId;
+  private int productId;
+  private String buyerEmail;
+  private int count;
+  private String state;
+  private int billId;
 
-	public Order() {
-	}
+  public Order() {
+  }
 
-	public Order(int productId, String buyerEmail, int count, String state, int billId) {
-		this.productId = productId;
-		this.buyerEmail = buyerEmail;
-		this.count = count;
-		this.state = state;
-		this.billId = billId;
-	}
+  /**
+   * 註解起來就好了啦.
+   *
+   *
+   *
+   *
+   *
+   */
+  public Order(int productId, String buyerEmail, int count, String state, int billId) {
+    this.productId = productId;
+    this.buyerEmail = buyerEmail;
+    this.count = count;
+    this.state = state;
+    this.billId = billId;
+  }
 
 
-	public int getproductId() {
-		return productId;
-	}
+  public int getproductId() {
+    return productId;
+  }
 
-	public void setproductId(int productId) {
-		this.productId = productId;
-	}
+  public void setproductId(int productId) {
+    this.productId = productId;
+  }
 
-	public String getbuyerEmail() {
-		return buyerEmail;
-	}
+  public String getbuyerEmail() {
+    return buyerEmail;
+  }
 
-	public void setbuyerEmail(String buyerEmail) {
-		this.buyerEmail = buyerEmail;
-	}
+  public void setbuyerEmail(String buyerEmail) {
+    this.buyerEmail = buyerEmail;
+  }
 
-	public int getcount() {
-		return count;
-	}
+  public int getcount() {
+    return count;
+  }
 
-	public void setcount(int count) {
-		this.count = count;
-	}
+  public void setcount(int count) {
+    this.count = count;
+  }
 
-	public String getState() {
-		return state;
-	}
+  public String getState() {
+    return state;
+  }
 
-	public void setState(String state) {
-		this.state = state;
-	}
+  public void setState(String state) {
+    this.state = state;
+  }
 
-	public int getBillId() {
-		return billId;
-	}
+  public int getBillId() {
+    return billId;
+  }
 
-	public void setBillId(int billId) {
-		this.billId = billId;
-	}
+  public void setBillId(int billId) {
+    this.billId = billId;
+  }
 }
 
