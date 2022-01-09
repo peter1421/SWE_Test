e4d9d0cae4bccd9f9a38f9604774e611b33dd0ae
mark46wei <ohbom292@gmail.com>
2022-01-09 19:09:45 +0800

    Checkstyle修正Account格式

--- src/main/java/fcu/sep/fcushop/model/Account.java
+++ src/main/java/fcu/sep/fcushop/model/Account.java
@@ -1,35 +1,43 @@
 package fcu.sep.fcushop.model;
 
+/**
+ * 註解起來就好了啦.
+ *
+ *
+ *
+ *
+ *
+ */
 public class Account {
 
-	private String email;
+  private String email;
 
-	private String password;
+  private String password;
 
 
-	public Account() {
-	}
+  public Account() {
+  }
 
-	public Account(String email, String password) {
-		this.email = email;
-		this.password = password;
-	}
+  public Account(String email, String password) {
+    this.email = email;
+    this.password = password;
+  }
 
-	public String getEmail() {
-		return email;
-	}
+  public String getEmail() {
+    return email;
+  }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
-	public String getPassword() {
-		return password;
-	}
+  public String getPassword() {
+    return password;
+  }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+  public void setPassword(String password) {
+    this.password = password;
+  }
 
 
 }
