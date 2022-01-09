a4a8c39e67acc3887e14e38f89b4b9ac93f0d836
mark46wei <ohbom292@gmail.com>
2022-01-09 17:28:38 +0800

    Checkstyle修正TestController格式_2

--- src/main/java/fcu/sep/fcushop/controller/TestController.java
+++ src/main/java/fcu/sep/fcushop/controller/TestController.java
@@ -1,8 +1,6 @@
 
 package fcu.sep.fcushop.controller;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,22 +11,40 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 註解起來就好了啦.
- *
- *
- *
+ * Checkstyle 說沒用到的
+ * import java.io.IOException;
+ * import javax.servlet.ServletException;
  *
  *
  */
 @RestController
 public class TestController {
 
+  /**
+   * 註解起來就好了啦.
+   *
+   *
+   *@param name ...
+   *@return tag
+   *
+   */
   @GetMapping("/greeting")
-  public String sayHello(@RequestParam String name) {
+  public String sayHello(@RequestParam final String name) {
     return "Hello " + name;
   }
 
+  /**
+   * 註解起來就好了啦.
+   *
+   *
+   *@param req ...
+   *@param resp ...
+   *
+   *
+   */
   @GetMapping("/api/cookie1")
-  protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+  protected void doPost(final HttpServletRequest req,
+                        final HttpServletResponse resp) {
     //創建cookie
     Cookie cookie = new Cookie("name", "zhangsan");
     resp.addCookie(cookie);
