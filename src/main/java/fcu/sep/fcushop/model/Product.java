78a75e627ceccc18b5d718795003bdb8336d34d4
mark46wei <ohbom292@gmail.com>
2022-01-09 21:24:51 +0800

    Checkstyle修正Product格式

--- src/main/java/fcu/sep/fcushop/model/Product.java
+++ src/main/java/fcu/sep/fcushop/model/Product.java
@@ -1,76 +1,97 @@
 package fcu.sep.fcushop.model;
 
+/**
+ * 註解起來就好了啦.
+ *
+ *
+ *
+ *
+ *
+ */
 public class Product {
 
-	private long id;
+  private long id;
 
-	private String name;
+  private String name;
 
-	private String imageUrl;
+  private String imageUrl;
 
-	private int price;
+  private int price;
 
-	private String description;
+  private String description;
 
-	private String classification;
+  private String classification;
 
-	public Product() {
-	}
+  public Product() {
+  }
 
-	public Product(long id, String name, String imageUrl, int price, String description, String classification) {
-		this.id = id;
-		this.name = name;
-		this.imageUrl = imageUrl;
-		this.price = price;
-		this.description = description;
-		this.classification = classification;
-	}
+  /**
+   * 註解起來就好了啦.
+   *
+   *
+   *
+   *
+   *
+   */
+  public Product(long id,
+                 String name,
+                 String imageUrl,
+                 int price,
+                 String description,
+                 String classification) {
+    this.id = id;
+    this.name = name;
+    this.imageUrl = imageUrl;
+    this.price = price;
+    this.description = description;
+    this.classification = classification;
+  }
 
-	public long getId() {
-		return id;
-	}
+  public long getId() {
+    return id;
+  }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+  public void setId(long id) {
+    this.id = id;
+  }
 
-	public String getName() {
-		return name;
-	}
+  public String getName() {
+    return name;
+  }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+  public void setName(String name) {
+    this.name = name;
+  }
 
-	public String getImageUrl() {
-		return imageUrl;
-	}
+  public String getImageUrl() {
+    return imageUrl;
+  }
 
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
+  public void setImageUrl(String imageUrl) {
+    this.imageUrl = imageUrl;
+  }
 
-	public int getPrice() {
-		return price;
-	}
+  public int getPrice() {
+    return price;
+  }
 
-	public void setPrice(int price) {
-		this.price = price;
-	}
+  public void setPrice(int price) {
+    this.price = price;
+  }
 
-	public String getDescription() {
-		return description;
-	}
+  public String getDescription() {
+    return description;
+  }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+  public void setDescription(String description) {
+    this.description = description;
+  }
 
-	public String getClassification() {
-		return classification;
-	}
+  public String getClassification() {
+    return classification;
+  }
 
-	public void setClassification(String classification) {
-		this.classification = classification;
-	}
+  public void setClassification(String classification) {
+    this.classification = classification;
+  }
 }
