e9abbd9947608759500536c483d5808ea484a726
mark46wei <ohbom292@gmail.com>
2022-01-09 19:52:04 +0800

    Checkstyle修正Bill格式

--- src/main/java/fcu/sep/fcushop/model/Bill.java
+++ src/main/java/fcu/sep/fcushop/model/Bill.java
@@ -1,42 +1,64 @@
 package fcu.sep.fcushop.model;
 
+/**
+ * 註解起來就好了啦.
+ *
+ *
+ *
+ *
+ *
+ */
 public class Bill extends Product {
-	private int count;
-	private int sum;
-	private String state;
+  private int count;
+  private int sum;
+  private String state;
 
-	public Bill() {
+  public Bill() {
 
-	}
+  }
 
-	public Bill(String imageUrl, String name, int count, int price, String classification, int sum, String state) {
-		this.count = count;
-		this.sum = sum;
-		this.state = state;
-	}
+  /**
+   * 註解起來就好了啦.
+   *
+   *
+   *
+   *
+   *
+   */
+  public Bill(String imageUrl,
+              String name,
+              int count,
+              int price,
+              String classification,
+              int sum,
+              String state) {
+    this.count = count;
+    this.sum = sum;
+    this.state = state;
+  }
 
-	public int getCount() {
-		return count;
-	}
+  public int getCount() {
+    return count;
+  }
 
-	public void setCount(int count) {
-		this.count = count;
-	}
+  public void setCount(int count) {
+    this.count = count;
+  }
 
-	public String getState() {
-		return state;
-	}
+  public String getState() {
+    return state;
+  }
 
-	public void setState(String state) {
-		this.state = state;
-	}
+  public void setState(String state) {
+    this.state = state;
+  }
 
-	public int getSum() {
-		return sum;
-	}
+  public int getSum() {
+    return sum;
+  }
 
-	public void setSum(int sum) {
-		this.sum = sum;
-	}
+  public void setSum(int sum) {
+    this.sum = sum;
+  }
 
 }
