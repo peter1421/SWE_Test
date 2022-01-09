9021c86a03a6b2705e6ca722f4e2bac650e0c860
mark46wei <ohbom292@gmail.com>
2022-01-09 21:04:35 +0800

    Checkstyle修正MemberBill格式_2

--- src/main/java/fcu/sep/fcushop/model/MemberBill.java
+++ src/main/java/fcu/sep/fcushop/model/MemberBill.java
@@ -9,33 +9,104 @@ package fcu.sep.fcushop.model;
  *
  */
 public class MemberBill extends Member {
+  /**
+   * 註解起來就好了啦.
+   *
+   *
+   *
+   *
+   *
+   */
   private String billId;
+  /**
+   * 註解起來就好了啦.
+   *
+   *
+   *
+   *
+   *
+   */
   private String state;
 
 
+  /**
+   * 註解起來就好了啦.
+   *
+   *
+   *
+   *
+   *
+   */
   public MemberBill() {
 
   }
 
-
-  public MemberBill(String billId, String imageUrl, String name, String address, String state) {
+  /**
+   * 註解起來就好了啦.
+   *
+   @param billId ...
+   *@param name ...
+   *@param imageUrl ...
+   *@param address ...
+   *@param state ...
+   *
+   *
+   *
+   */
+  public MemberBill(final String billId,
+                    final String imageUrl,
+                    final String name,
+                    final String address,
+                    final String state) {
     this.billId = billId;
     this.state = state;
   }
 
+  /**
+   * 註解起來就好了啦.
+   *
+   *
+   *@return tag
+   *
+   *
+   */
   public String getState() {
     return state;
   }
 
-  public void setState(String state) {
+  /**
+   * 註解起來就好了啦.
+   *
+   *
+   *@param state ...
+   *
+   *
+   */
+  public void setState(final String state) {
     this.state = state;
   }
 
+  /**
+   * 註解起來就好了啦.
+   *
+   *
+   *@return tag
+   *
+   *
+   */
   public String getbillId() {
     return billId;
   }
 
-  public void setbillId(String billId) {
+  /**
+   * 註解起來就好了啦.
+   *
+   *
+   *@param billId ...
+   *
+   *
+   */
+  public void setbillId(final String billId) {
     this.billId = billId;
   }
 
