dd335c37cd90eb8d3929d819f10164533ab6288c
mark46wei <ohbom292@gmail.com>
2022-01-09 20:08:34 +0800

    Checkstyle修正BuyerOrder格式

--- src/main/java/fcu/sep/fcushop/model/BuyerOrder.java
+++ src/main/java/fcu/sep/fcushop/model/BuyerOrder.java
@@ -1,57 +1,78 @@
 package fcu.sep.fcushop.model;
 
-public class BuyerOrder extends Order{
-	private int orderID;
-	private String name;
-	private int price;
-	private String description;
+/**
+ * 註解起來就好了啦.
+ *
+ *
+ *
+ *
+ *
+ */
+public class BuyerOrder extends Order {
+  private int orderID;
+  private String name;
+  private int price;
+  private String description;
 
 
-	public BuyerOrder() {
+  public BuyerOrder() {
 
-	}
+  }
 
-	public BuyerOrder(String buyerEmail, int orderID, int count, String name, int price, String description) {
-		this.orderID = orderID;
-		this.name = name;
-		this.price = price;
-		this.description = description;
-	}
+  /**
+   * 註解起來就好了啦.
+   *
+   *
+   *
+   *
+   *
+   */
+  public BuyerOrder(String buyerEmail,
+                    int orderID,
+                    int count,
+                    String name,
+                    int price,
+                    String description) {
+    this.orderID = orderID;
+    this.name = name;
+    this.price = price;
+    this.description = description;
+  }
 
-	public int getorderID() {
-		return orderID;
-	}
+  public int getorderID() {
+    return orderID;
+  }
 
-	public void setorderID(int orderID) {
-		this.orderID = orderID;
-	}
-
-
-
-	public int getprice() {
-		return price;
-	}
-
-	public void setprice(int price) {
-		this.price = price;
-	}
+  public void setorderID(int orderID) {
+    this.orderID = orderID;
+  }
 
 
-	public String getname() {
-		return name;
-	}
 
-	public void setname(String name) {
-		this.name = name;
-	}
+  public int getprice() {
+    return price;
+  }
 
-	public String description() {
-		return description;
-	}
+  public void setprice(int price) {
+    this.price = price;
+  }
 
-	public void setdescription(String description) {
-		this.description = description;
-	}
+
+  public String getname() {
+    return name;
+  }
+
+  public void setname(String name) {
+    this.name = name;
+  }
+
+  public String description() {
+    return description;
+  }
+
+  public void setdescription(String description) {
+    this.description = description;
+  }
 
 
 }
