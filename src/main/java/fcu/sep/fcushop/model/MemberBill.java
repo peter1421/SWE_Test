69ebaf68fe5d905542d15c3b2eae133a8a4828ce
mark46wei <ohbom292@gmail.com>
2022-01-09 21:00:02 +0800

    Checkstyle修正MemberBill格式

--- src/main/java/fcu/sep/fcushop/model/MemberBill.java
+++ src/main/java/fcu/sep/fcushop/model/MemberBill.java
@@ -1,32 +1,42 @@
 package fcu.sep.fcushop.model;
 
-public class MemberBill extends Member{
-	private String billId;
-	private String state;
+/**
+ * 註解起來就好了啦.
+ *
+ *
+ *
+ *
+ *
+ */
+public class MemberBill extends Member {
+  private String billId;
+  private String state;
 
 
-	public MemberBill() {
+  public MemberBill() {
 
-	}
+  }
 
 
-	public MemberBill(String billId,String imageUrl,String name,String address,String state) {
-		this.billId=billId;
-		this.state = state;
-	}
-	public String getState() {
-		return state;
-	}
+  public MemberBill(String billId, String imageUrl, String name, String address, String state) {
+    this.billId = billId;
+    this.state = state;
+  }
 
-	public void setState(String state) {
-		this.state = state;
-	}
-	public String getbillId() {
-		return billId;
-	}
+  public String getState() {
+    return state;
+  }
 
-	public void setbillId(String billId) {
-		this.billId = billId;
-	}
+  public void setState(String state) {
+    this.state = state;
+  }
+
+  public String getbillId() {
+    return billId;
+  }
+
+  public void setbillId(String billId) {
+    this.billId = billId;
+  }
 
 }
