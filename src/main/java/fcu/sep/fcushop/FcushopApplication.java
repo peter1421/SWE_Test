708239540c818881af3aa8d85737b89b2d21601c
mark46wei <ohbom292@gmail.com>
2022-01-09 21:40:42 +0800

    Checkstyle修正FcushopApplication格式_2

--- src/main/java/fcu/sep/fcushop/FcushopApplication.java
+++ src/main/java/fcu/sep/fcushop/FcushopApplication.java
@@ -18,11 +18,11 @@ public class FcushopApplication {
    * 註解起來就好了啦.
    *
    *
-   *
+   *@param args ...
    *
    *
    */
-  public static void main(String[] args) {
+  public static void main(final String[] args) {
 
     SpringApplication.run(FcushopApplication.class, args);
     System.out.println("-------------------------------------------");
@@ -30,7 +30,9 @@ public class FcushopApplication {
     System.out.println("網頁:http://localhost:8080/index.html");
     System.out.println("網頁:http://localhost:8083/");
     System.out.println("重整瀏覽器:chrome://settings/privacy");
-    System.out.println("賣家_商品添加:http://localhost:8080/Seller/form-layouts.html");
+    System.out.println(""
+            +
+            "賣家_商品添加:http://localhost:8080/Seller/form-layouts.html");
     System.out.println("買家_商品瀏覽:http://localhost:8080/food_2.html");
     //netstat -ano|findstr 8080
   }
